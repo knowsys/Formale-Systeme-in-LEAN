7e0a30dfb0d855d160466b7cebde4bcf2873d023
Matthias Meissner <geige.matze@gmail.com>
2023-05-31 00:13:38 +0200

    begin proof regular grammar -> nfa

--- FormalSystems/Chomsky/Regular/NFA.lean
+++ FormalSystems/Chomsky/Regular/NFA.lean
@@ -16,6 +16,20 @@ def accept_from (M: NFA α qs) (current: Finset M.Q) : Word M.Z → List M.Q →
   | w :: ws, x :: xs => x ∈ current ∧ M.accept_from (M.δ (x, w)) ws xs
   | _, _ => False
 
+theorem accept_from_empty : accept_from M current word [] = False := by 
+  unfold accept_from
+  split
+  . contradiction
+  . contradiction
+  . rfl
+
+theorem accept_from_contains (h: accept_from M current word (x :: xs)) : x ∈ current := by
+  unfold accept_from at h
+  split at h
+  case h_1 h eq => exact (List.cons_eq_cons.mp eq).1 ▸ h.1
+  case h_2 h eq => exact (List.cons_eq_cons.mp eq).1 ▸ h.1
+  case h_3 => apply False.elim; assumption
+
 def GeneratedLanguage (M: NFA α qs) : Language M.Z :=
   fun w => ∃run: List M.Q, M.accept_from M.Q₀ w run
 
@@ -59,6 +73,57 @@ def RegularGrammar.toNFA (G: RegularGrammar α nt) : NFA α (G.V ⊕ SingAccepti
 variable (G: RegularGrammar α nt)
 def M := RegularGrammar.toNFA G
 
+theorem nfa_run_to_derivation_1 (runh: (M G).accept_from (M G).Q₀ w (⟨ .inl x, p ⟩ :: xs)) :
+  [.inl x] (G)⇒* (.inr <$> w) := by
+  match xs, w with
+  -- in this case there is a rule S -> ε
+  | [], [] =>
+    cases runh
+    case intro _ r =>
+      unfold M RegularGrammar.toNFA at r; simp at r
+      simp; sorry
+
+  -- this cannot happen
+  | [], w :: ws => sorry
+
+  -- a rule S -> a
+  | [ ⟨ .inr _ , _ ⟩ ], [ a ] => sorry
+
+  -- finally a rule S -> w x'
+  | x' :: xs, w :: ws => sorry
+
+theorem nfa_run_to_derivation_2 (runh: (M G).accept_from ((M G).δ (q, a)) w (⟨ .inl x, p ⟩ :: xs)) :
+  [.inl x] (G)⇒* (.inr <$> w) := by
+  match xs, w with
+  -- in this case there is a rule A -> ε
+  | [], [] =>
+    cases runh
+    case intro l r => sorry
+
+  -- this cannot happen
+  | [], w :: ws => sorry
+
+  -- a rule A -> a
+  | [ ⟨ .inr _ , _ ⟩ ], [ a ] => sorry
+
+  -- finally a rule A -> w x'
+  | x' :: xs, w :: ws => sorry
+
 theorem nfa_lang_subs_grammar : (M G).GeneratedLanguage ⊆ G.GeneratedLanguage := by
   intro word ⟨ run, runh ⟩
-  sorry
+  match run with
+  -- empty runs are not valid
+  | [] => apply False.elim; exact NFA.accept_from_empty ▸ runh 
+
+  -- in this case the run would begin with the single accepting state, which is not in Q₀
+  | ⟨ .inr _, _ ⟩ :: _ =>
+    have p := NFA.accept_from_contains runh
+    unfold M RegularGrammar.toNFA at p; simp at p
+
+  -- this is the interesting case, where v ∈ Q₀ and thus v = G.start
+  | ⟨ .inl v, _ ⟩ :: _ =>
+      have p := NFA.accept_from_contains runh
+      unfold M RegularGrammar.toNFA at p; simp at p
+      have tmp := nfa_run_to_derivation_1 G runh
+      rw [p] at tmp
+      exact tmp
