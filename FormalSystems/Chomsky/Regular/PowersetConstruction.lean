5dcb5ad51ce1c5d618559bf23c7608e97700c2dc
Matthias Meissner <geige.matze@gmail.com>
2023-08-04 15:07:47 +0200

    finish powerset construction by proving L(DFA.fromNFA) = L(NFA)

--- FormalSystems/Chomsky/Regular/PowersetConstruction.lean
+++ FormalSystems/Chomsky/Regular/PowersetConstruction.lean
@@ -102,3 +102,43 @@ theorem NFA.lang_subs_DFA_fromNFA_lang:
   apply DFA.fromNFA.RunFromRestricted_final
   apply DFA.fromNFA.NFARunToRun_final_imp_final
   assumption
+
+theorem DFA.fromNFA_run_imp_run
+  { q₀: (DFA.fromNFA M).Q }
+  { r: (DFA.fromNFA M).Run q₀ w } (h: r.last ∈ (DFA.fromNFA M).F):
+  ∃q ∈ q₀.val, ∃r': M.Run q w, r'.last ∈ M.F := by
+  match r with
+  | .final _ _ =>
+    dsimp [NFA.Run.last, fromNFA] at h
+    have ⟨_, qf, _, _⟩ := Finset.mem_filter.mp h
+    exists qf; constructor; assumption
+    apply Exists.intro; swap
+    apply NFA.Run.final _
+    assumption
+    dsimp [NFA.Run.last]
+    assumption
+  | .step _ h_step _ _ =>
+    dsimp [NFA.Run.last] at h
+    dsimp [fromNFA, toNFA] at h_step; simp at h_step
+    have ⟨_, h_q₂, r', hr'⟩ := fromNFA_run_imp_run h
+    rw [h_step, Finset.mem_fold_union_iff] at h_q₂
+    have ⟨x, _, _⟩ := h_q₂
+    exists x; constructor; assumption
+    apply Exists.intro; swap
+    apply NFA.Run.step
+    repeat { assumption }
+
+theorem NFA.DFA_fromNFA_lang_subs_lang:
+  (DFA.fromNFA M).GeneratedLanguage ⊆ M.GeneratedLanguage := by
+  intro w ⟨_, hr⟩
+  have ⟨q, hq, r, _⟩ := DFA.fromNFA_run_imp_run hr
+  exists q
+  dsimp [DFA.fromNFA] at hq
+  constructor; assumption
+  exists r
+
+theorem NFA.fromNFA_lang_eq_lang:
+  (DFA.fromNFA M).GeneratedLanguage = M.GeneratedLanguage := by
+  apply Set.ext; intros; constructor
+  apply DFA_fromNFA_lang_subs_lang
+  apply lang_subs_DFA_fromNFA_lang
