67d07ef9ab93c9499d8bd6172dcde114608f746d
Matthias Meissner <geige.matze@gmail.com>
2023-11-15 15:30:17 +0100

    setup ci

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "FormalSystems lean implementation";
+
+  inputs.lean.url = "github:leanprover/lean4";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, lean, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      leanPkgs = lean.packages.${system};
+      pkg = leanPkgs.buildLeanPackage {
+        name = "FormalSystems";
+        src = ./.;
+      };
+    in {
+      packages = pkg // {
+        inherit (leanPkgs) lean;
+      };
+
+      defaultPackage = pkg.modRoot;
+    });
+}
