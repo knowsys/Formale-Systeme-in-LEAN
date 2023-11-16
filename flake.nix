3aa91e77ac5b077850842aecaccec672522ae64e
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-11-16 23:18:40 +0100

    Use alejandra as nix formatter

--- flake.nix
+++ flake.nix
@@ -13,68 +13,80 @@
     };
   };
 
-  outputs = inputs@{ self, flake-utils, nixpkgs, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        pkgs = import nixpkgs { system = system; };
-        leanPkgs = inputs.lean.packages.${system};
+  outputs = inputs @ {
+    self,
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
+      pkgs = import nixpkgs {system = system;};
+      leanPkgs = inputs.lean.packages.${system};
 
-        overrides = {
-          std.name = "Std";
-          aesop.name = "Aesop";
-          mathlib.name = "Mathlib";
-          proofwidgets.name = "ProofWidgets";
-        };
+      overrides = {
+        std.name = "Std";
+        aesop.name = "Aesop";
+        mathlib.name = "Mathlib";
+        proofwidgets.name = "ProofWidgets";
+      };
 
-        inherit (pkgs) lib;
-        inherit (leanPkgs) buildLeanPackage;
-        inherit (lib) mapAttrs attrValues;
+      inherit (pkgs) lib;
+      inherit (leanPkgs) buildLeanPackage;
+      inherit (lib) mapAttrs attrValues;
 
-        dependenciesFromLakeManifest = path:
-          let
-            inherit (builtins) fromJSON readFile fetchGit pathExists;
-            inherit (lib) listToAttrs nameValuePair optionalAttrs;
+      dependenciesFromLakeManifest = path: let
+        inherit (builtins) fromJSON readFile fetchGit pathExists;
+        inherit (lib) listToAttrs nameValuePair optionalAttrs;
 
-            manifest = fromJSON (readFile path);
-            lakePackages = listToAttrs
-              (map (pkg: nameValuePair pkg.git.name pkg.git) manifest.packages);
+        manifest = fromJSON (readFile path);
+        lakePackages =
+          listToAttrs
+          (map (pkg: nameValuePair pkg.git.name pkg.git) manifest.packages);
 
-            fetchLakePackage = name:
-              fetchGit {
-                inherit name;
-                inherit (lakePackages."${name}") url rev;
-              };
+        fetchLakePackage = name:
+          fetchGit {
+            inherit name;
+            inherit (lakePackages."${name}") url rev;
+          };
 
-            buildLakePackage = name:
-              let
-                src = fetchLakePackage name;
+        buildLakePackage = name: let
+          src = fetchLakePackage name;
 
-                inner-manifest = "${src}/lake-manifest.json";
-                deps = (optionalAttrs (pathExists inner-manifest) {
-                  deps =
-                    attrValues (dependenciesFromLakeManifest inner-manifest);
-                });
+          inner-manifest = "${src}/lake-manifest.json";
+          deps = optionalAttrs (pathExists inner-manifest) {
+            deps =
+              attrValues (dependenciesFromLakeManifest inner-manifest);
+          };
 
-                package = buildLeanPackage ({ inherit name src; } // deps);
-              in package.override (overrides."${name}" or { });
-          in mapAttrs (name: _: buildLakePackage name) lakePackages;
+          package = buildLeanPackage ({inherit name src;} // deps);
+        in
+          package.override (overrides."${name}" or {});
+      in
+        mapAttrs (name: _: buildLakePackage name) lakePackages;
 
-        dependencies = dependenciesFromLakeManifest ./lake-manifest.json;
-        lakePkgs = mapAttrs (_: value: value.modRoot) dependencies;
+      dependencies = dependenciesFromLakeManifest ./lake-manifest.json;
+      lakePkgs = mapAttrs (_: value: value.modRoot) dependencies;
 
-        package = buildLeanPackage {
-          name = "FormalSystems";
-          src = ./.;
-          deps = attrValues dependencies;
-        };
-      in {
-        packages = lakePkgs // (rec {
+      package = buildLeanPackage {
+        name = "FormalSystems";
+        src = ./.;
+        deps = attrValues dependencies;
+      };
+    in {
+      packages =
+        lakePkgs
+        // rec {
           inherit (leanPkgs) lean lean-all;
 
           FormalSystems = package.modRoot;
           default = FormalSystems;
-        });
+        };
 
-        devShells.default = package.devShell;
-      });
+      devShells.default = package.devShell;
+
+      formatter = pkgs.alejandra;
+    });
 }
+# Local Variables:
+# apheleia-formatter: alejandra
+# End:
