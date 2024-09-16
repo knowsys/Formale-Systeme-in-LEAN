c8e86b9dc5ce5d17c9512e9d0c61d5632940af6a
monsterkrampe <monsterkrampe@users.noreply.github.com>
2024-09-16 09:24:29 +0200

    Fix description in flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Ruleset Classifier";
+  description = "FormalSystems lean implementation";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
