17b6e6555833d0d76c227bfdc64a5569cff2b898
monsterkrampe <monsterkrampe@users.noreply.github.com>
2024-09-16 09:17:31 +0200

    Only use flake to provide dev shell with elan

--- flake.nix
+++ flake.nix
@@ -1,93 +1,19 @@
 {
-  description = "FormalSystems lean implementation";
+  description = "Ruleset Classifier";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    flake-utils.url = "github:numtide/flake-utils";
-    lean = {
-      url = "github:leanprover/lean4/v4.2.0";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = inputs @ {
-    self,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import inputs.nixpkgs {system = system;};
-      leanPkgs = inputs.lean.packages.${system};
-
-      overrides = {
-        std.name = "Std";
-        aesop.name = "Aesop";
-        mathlib.name = "Mathlib";
-        proofwidgets.name = "ProofWidgets";
-      };
-
-      inherit (pkgs) lib;
-      inherit (leanPkgs) buildLeanPackage;
-      inherit (lib) mapAttrs attrValues;
-
-      dependenciesFromLakeManifest = path: let
-        inherit (builtins) fromJSON readFile fetchGit pathExists;
-        inherit (lib) listToAttrs nameValuePair optionalAttrs;
-
-        manifest = fromJSON (readFile path);
-        lakePackages =
-          listToAttrs
-          (map (pkg: nameValuePair pkg.git.name pkg.git) manifest.packages);
-
-        fetchLakePackage = name:
-          fetchGit {
-            inherit name;
-            inherit (lakePackages."${name}") url rev;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.elan
+            ];
           };
-
-        buildLakePackage = name: let
-          src = fetchLakePackage name;
-
-          inner-manifest = "${src}/lake-manifest.json";
-          deps = optionalAttrs (pathExists inner-manifest) {
-            deps =
-              attrValues (dependenciesFromLakeManifest inner-manifest);
-          };
-
-          package = buildLeanPackage ({inherit name src;} // deps);
-        in
-          package.override (overrides."${name}" or {});
-      in
-        mapAttrs (name: _: buildLakePackage name) lakePackages;
-
-      dependencies = dependenciesFromLakeManifest ./lake-manifest.json;
-      lakePkgs = mapAttrs (_: value: value.modRoot) dependencies;
-
-      package = buildLeanPackage {
-        name = "FormalSystems";
-        src = ./.;
-        deps = attrValues dependencies;
-      };
-    in {
-      packages =
-        lakePkgs
-        // rec {
-          inherit (leanPkgs) lean lean-all;
-
-          FormalSystems = package.modRoot;
-          default = FormalSystems;
-        };
-
-      devShells.default = package.devShell.overrideAttrs (old: {
-        buildInputs = [leanPkgs.lean-all] ++ (old.buildInputs or []);
-      });
-
-      formatter = pkgs.alejandra;
-    });
+        }
+      );
 }
-# Local Variables:
-# apheleia-formatter: alejandra
-# End:
